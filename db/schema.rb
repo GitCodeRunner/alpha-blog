4905f268485d158f6718d250b0e93f04753866a8
recode now <recodenow@gmail.com>
2016-07-12 14:02:43 +0000

    add sign in upon signup and add admin user functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710155635) do
+ActiveRecord::Schema.define(version: 20160712135041) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160710155635) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
